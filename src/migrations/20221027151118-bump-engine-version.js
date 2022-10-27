b4391ccd8bcb2efa3e3fdf2b1afc2ac12d6c2008
Kai <kai@guardrails.io>
2022-10-27 15:11:19 +0700

    my commit message

---
+++ src/migrations/20221027151118-bump-engine-version.js
@@ -0,0 +1,7 @@
+export const up = queryInterface => queryInterface.sequelize.query(` 
+UPDATE "Engines" SET version='v1.1.1', "updatedAt"=NOW() WHERE name='test';
+UPDATE "Engines" SET version='v1.1.2', "updatedAt"=NOW() WHERE name='test_1'; `); 
+export const down = queryInterface =>
+queryInterface.sequelize.query(` 
+UPDATE "Engines" SET version='v1.0.0', "updatedAt"=NOW() WHERE name='test';
+UPDATE "Engines" SET version='v1.1.1', "updatedAt"=NOW() WHERE name='test_1'; `);
